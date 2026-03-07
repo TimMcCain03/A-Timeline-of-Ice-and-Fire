159db0f51795f4907c6f4d09ffae903fc1b2ce3b
TimMcCain03 <tm3mcc@gmail.com>
2026-03-06 17:34:42 -0800

    trying again for initial character tracking

---
+++ src/data/characterTracking.ts
@@ -0,0 +1,18 @@
+// import { chapters } from "./chapters"
+// import { locations } from "./locations"
+// import type { Location } from "./locations"
+
+export const characterIcons: Record<string, string> = {
+  'Eddard Stark': '/icons/eddard-stark.png',
+  'Catelyn Stark': '/icons/catelyn-stark.png',
+  'Sansa Stark': '/icons/sansa-stark.png',
+  'Arya Stark': '/icons/arya-stark.png',
+  'Bran Stark': '/icons/bran-stark.png',
+  'Jon Snow': '/icons/jon-snow.png',
+  'Daenerys Targaryen': '/icons/daenerys-targaryen.png',
+  'Tyrion Lannister': '/icons/tyrion-lannister.png',
+  'Jaime Lannister': '/icons/jaime-lannister.png',
+  'Cersei Lannister': '/icons/cersei-lannister.png',
+  // more povs added here
+}
+
